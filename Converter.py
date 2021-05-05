e190c2b253e5a362f97e4bbc064c26cf9748173e
vardanaloyan <vardan.aloyan@powerdata.am>
2021-05-06 00:21:45 +0400

    finetuned

--- Converter.py
+++ Converter.py
@@ -1,27 +1,50 @@
 #!/usr/bin/env python3
-import os, sys
+import os
+import sys
 
-sep = "," # SEPERATOR FOR OUTPUT FILE . For tab use sep='"\t"'
+sep = ","  # SEPARATOR FOR OUTPUT FILE . For tab use sep='"\t"'
 schema = "input.csv"
 
-def readFields(file):
-    urls_list = []
+
+def read_fields(file):
+    """
+    :param file: filepath
+    :type file: str
+    :return: list of fields specified in scheme
+    :rtype: list
+    """
     with open(file, 'r') as f:
         _fields_str = f.read()
     _fields_lst = _fields_str.strip().split(",")
     _fields_lst = [_str.strip() for _str in _fields_lst]
     return _fields_lst
 
-def Construct(filename, dest, _fields, sep):
+
+def construct(filename, dest, _fields, sep):
+    """
+    This function constructs and returns valid tshark command
+    :param filename: filename
+    :type filename: str
+    :param dest: output file
+    :type dest: str
+    :param _fields: extracted fields
+    :type _fields: list
+    :param sep: separator
+    :type sep: str
+    :return: tshark command
+    :rtype: str
+    """
     fields = "-e "+" -e ".join(_fields)
     cmd = 'tshark -r %s -T fields %s -E separator=%s -E occurrence=f > %s' % (filename, fields, sep, dest)
     return cmd
 
-if len(sys.argv) != 3:
-    print ("Need to specify 2 commands\nExample python3 Converter.py xxx.pcap output.log")
-else:
-    fields = readFields(schema)
-    filename = sys.argv[1]
-    dest = sys.argv[2]
-    cmd = Construct(filename, dest, fields, sep)
-    os.system(cmd)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Need to specify 2 commands\nExample python3 Converter.py xxx.pcap output.log")
+    else:
+        fields = read_fields(schema)
+        filename = sys.argv[1]
+        dest = sys.argv[2]
+        cmd = construct(filename, dest, fields, sep)
+        os.system(cmd)
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # python-pcap-extractor
-Extract pcap files using tshark
+## Extract pcap files using tshark
+
+For this script dependency is tshark app which can be installed on linux by following command
+
+```
+sudo apt install tshark
+```
+
+In order to run the script you have to give input schema in `input.csv` file
+
+Run the script by following command
+
+```
+python3 Converter.py modbus.pcap out.csv
+```
